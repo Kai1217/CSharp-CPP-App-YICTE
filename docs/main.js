b14177aba59c201e6fba09d4aa936f7a449054f8
Kai Matolat <kaimatolat@hotmail.com>
2018-12-05 19:04:12 +1000

    Added JS Function getOS() a and allowed for a log to be parsed to the console with console.log

--- docs/main.js
+++ docs/main.js
@@ -2,4 +2,10 @@ function getOS()
 {
   var OSName = "Unidentified Operating System";
   if (window.navigator.userAgent.indexOf("Android") != -1) OSName="Android";
+
+  if (OSName = "Android")
+  {
+    console.log("You are on "+OSName);
+    // TODO -> DISPLAY A MESSAGE ON THE WEBPAGE
+  }
 } 
