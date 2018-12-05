e98ad1b0d907c14e585c2c7a2225da11aac02682
Kai Matolat <kaimatolat@hotmail.com>
2018-12-05 19:13:53 +1000

    Changed a logical error in getOS function

--- docs/main.js
+++ docs/main.js
@@ -3,9 +3,9 @@ function getOS()
   var OSName = "Unidentified Operating System";
   if (window.navigator.userAgent.indexOf("Android") != -1) OSName="Android";
 
-  if (OSName = "Android")
+  if (OSName == "Android")
   {
-    console.log("You are on "+OSName);
+    alert("You are on "+OSName);
     // TODO -> DISPLAY A MESSAGE ON THE WEBPAGE
   }
 } 
